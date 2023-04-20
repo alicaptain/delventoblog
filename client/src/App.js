56cbf25791cf0ef974dba7c46d9427919f999e80
alicaptain <aligulerworks@gmail.com>
2023-04-20 16:27:39 +0300

    Just Another

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
 import "./App.css";
-import Header from "./Header";
+// import Header from "./Header";
 import Tale from "./Tale";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-
-            <div className="tales">
-              <Tale />
-              <Tale />
-            </div>
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Tale />} />
+        <Route path={"/login"} element={<div>Login Page</div>} />
+      </Route>
     </Routes>
   );
 }
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header>
       <nav2>
-        <a href="#">login</a>
-        <a href="#">get in</a>
+        <Link to="/login">login</Link>
+        <Link to="/getin">get in</Link>
       </nav2>
-      <a href="" className="logo">
+      <Link to="/" className="logo">
         ali del vento
-      </a>
+      </Link>
       <nav1>
-        <a href="/login">login</a>
-        <a href="/getin">get in</a>
+        <Link to="/login">login</Link>
+        <Link to="/getin">get in</Link>
       </nav1>
     </header>
   );
