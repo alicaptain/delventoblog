71245cb8994a209d4e6929a0f36f8d0cf25a7071
alicaptain <aligulerworks@gmail.com>
2023-04-20 14:40:43 +0300

    File Replacements

---
+++ client/src/Header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header>
+      <nav2>
+        <a href="#">login</a>
+        <a href="#">get in</a>
+      </nav2>
+      <a href="" className="logo">
+        ali del vento
+      </a>
+      <nav1>
+        <a href="/login">login</a>
+        <a href="/getin">get in</a>
+      </nav1>
+    </header>
+  );
+}
---
+++ client/src/Tale.js
@@ -0,0 +1,38 @@
+export default function Tale() {
+  return (
+    <div className="tale">
+      <h1>Her Turhlu</h1>
+      <p>
+        If only some of ‘you’ would be able to translate this to English
+        “procreatinoperly”, I’d believe that you’re at least as intelligent as
+        me.{" "}
+      </p>
+
+      <p>
+        This is Ali. Calling, some-times yelling out from centuriesfilled
+        information, wisdom and a thousand year of retardness aged like wine
+        with aromas and scents of mistakes and painful experiences. Houston!
+        Give up expecting life from Mars, it had more water then you can find
+        now when i was a child and there were still some crocodelic mammals
+        swimming in very deep of them. But cold weather is also not very good
+        when you get too much used to. And that cold weather liquadites the
+        atmosphere you breath. And when it’s up above the surface with a
+        centrifugal power of a rotating spherical surface which has less gravity
+        than the big neighbor where they start to leave with the help of time...
+        You can find their skeletons if you dig enough under the sand happened
+        after the hills got dry, no moist, no plants, no softness between hot
+        days and cold nights...
+      </p>
+      <p>
+        This is a cruel universe with really interesting gifts that can
+        disappear until you realize their value and how lovely were they ‘on
+        time’... I would like to ask, please, do watch out while knowing it will
+        be useless request.
+      </p>
+      <div className="tale-info">
+        <a className="writer">ali del vento</a>
+        <time>2023-04-19 11:11pm</time>
+      </div>
+    </div>
+  );
+}
