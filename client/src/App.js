eab82cc645fb95f1e5f8aecc4216584f910af748
alicaptain <aligulerworks@gmail.com>
2023-04-20 17:00:44 +0300

    Before Backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-// import Header from "./Header";
+import Header from "./Header";
 import Tale from "./Tale";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import GetinPage from "./pages/getinPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Tale />} />
-        <Route path={"/login"} element={<div>Login Page</div>} />
+        <Route index element={<HomePage />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/getin"} element={<GetinPage />} />
       </Route>
     </Routes>
   );
